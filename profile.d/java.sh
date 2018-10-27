be384534213cee6a6c4367098d87bb2befa47b01
Florian Limberger <florian.limberger@inovex.de>
2018-10-27 17:33:57 +0200

    Update config for 4k display
    - Use huge fonts for `urxvt(1)' and `xterm(1)'
    - Remove the Java fix for `dwm(1)'

--- profile.d/java.sh
+++ profile.d/java.sh
@@ -2,9 +2,6 @@
 # export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 export JAVA_HOME="/opt/google/android-studio/jre"
 
-# Fix Java in dwm
-export _JAVA_AWT_WM_NONREPARENTING=1
-
 # These were scrips before, now consolidated into $HOME/bin/sh/java_wrapper
 alias java7='java_wrapper -v 7'
 alias java8='java_wrapper -v 8'
