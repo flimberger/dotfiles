8fbc30fc322cc2092d5322bba9087b7590554b5e
Florian Limberger <flo@purplekraken.com>
2019-12-07 13:56:38 +0100

    Fix $NPROC environment variable on FreeBSD

--- profile.d/plan9.sh
+++ profile.d/plan9.sh
@@ -1,7 +1,7 @@
 # Plan9port installation directory
 export PLAN9=/usr/local/plan9
 case $(uname) in
-FreeBSD) NPROC=$(sysctl hw.ncpu) ;;
+FreeBSD) NPROC=$(sysctl -n hw.ncpu) ;;
 Linux) NPROC=$(nproc) ;;
 esac
 export NPROC
