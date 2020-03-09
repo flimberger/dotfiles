4f8744b0360dd1729a51320d843a7e32d810e947
Florian Limberger <florian.limberger@inovex.de>
2020-03-09 07:48:08 +0100

    Move android stuff
    Move android stuff from `$HOME/android` to `$HOME/lib/android`. Android
    Studio now lives at `$HOME/lib/apps/android-studio`, which is consistent
    with the other apps I installed in my home directory.

--- profile.d/android.sh
+++ profile.d/android.sh
@@ -1,7 +1,7 @@
-ANDROID_SDK_ROOT="$HOME/android/sdk"
-# This is deprecated, by still used by gradle
+ANDROID_SDK_ROOT="$HOME/lib/android/sdk"
+# This is deprecated, but still used by gradle
 ANDROID_HOME="$ANDROID_SDK_ROOT"
-ANDROID_STUDIO_HOME="$HOME/android/studio"
+ANDROID_STUDIO_HOME="$HOME/lib/apps/android-studio"
 PATH="${PATH}:${ANDROID_SDK_ROOT}/tools/bin:${ANDROID_SDK_ROOT}/tools:${ANDROID_SDK_ROOT}/platform-tools:${ANDROID_SDK_ROOT}/build-tools/$(ls "${ANDROID_SDK_ROOT}/build-tools" | sort -n | tail -1)"
 
 export ANDROID_HOME ANDROID_SDK_ROOT ANDROID_STUDIO_HOME PATH
--- profile.d/java.sh
+++ profile.d/java.sh
@@ -1,8 +1,3 @@
 # Older gradle versions do not support Java 10
 # export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
-export JAVA_HOME="$HOME/android/studio/jre"
-
-# These were scrips before, now consolidated into $HOME/bin/sh/java_wrapper
-alias java7='java_wrapper -v 7'
-alias java8='java_wrapper -v 8'
-alias java11='java_wrapper -v 11'
+export JAVA_HOME="$HOME/lib/apps/android-studio/jre"
