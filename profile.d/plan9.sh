25edfe0190c3bc60139cd6f01806f5f0ef5586c3
Florian Limberger <flo@purplekraken.com>
2020-11-28 19:57:15 +0100

    [profile] Make Plan 9 environment variables lowercase again
    `CPUTYPE` is also used by `/usr/share/mk/bsd.cpu.mk`, which is included
    in virtually all other makefiles. Using the lowercase variables works
    around this issue.

--- profile.d/plan9.sh
+++ profile.d/plan9.sh
@@ -1,5 +1,6 @@
 # Plan9port installation directory
 export PLAN9=/usr/local/plan9
+
 case $(uname) in
 FreeBSD) NPROC=$(sysctl -n hw.ncpu) ;;
 Linux) NPROC=$(nproc) ;;
@@ -7,7 +8,6 @@ esac
 export NPROC
 export PATH="$PATH:$PLAN9/bin"
 
-# Use uppercase variable names as customary on Unix.
-export SYSNAME=$(hostname -s)
-export CPUTYPE=$(uname -m)
-export OBJTYPE="$CPUTYPE"
+export sysname=$(hostname -s)
+export cputype=$(uname -m)
+export objtype="$cputype"
