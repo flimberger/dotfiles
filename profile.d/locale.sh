d0b609e0f08d53d6c9e6634a6964b437e7f6f406
Florian Limberger <flo@purplekraken.com>
2021-01-31 21:55:08 +0100

    Fix up locale to mix german and english

--- profile.d/locale.sh
+++ profile.d/locale.sh
@@ -1,2 +1,15 @@
-LANG="en_US.UTF-8"
+export LANG="en_GB.UTF-8"
+export LC_ADDRESS="de_DE.UTF-8"
+export LC_COLLATE="en_GB.UTF-8"
+export LC_CTYPE="en_GB.UTF-8"
+export LC_IDENTIFICATION="de_DE.UTF-8"
+export LC_MEASUREMENT="de_DE.UTF-8"
+export LC_MESSAGES="en_GB.UTF-8"
+export LC_MONETARY="de_DE.UTF-8"
+export LC_NAME="de_DE.UTF-8"
+export LC_NUMERIC="de_DE.UTF-8"
+export LC_PAPER="de_DE.UTF-8"
+export LC_TELEPHONE="de_DE.UTF-8"
+export LC_TIME="en_IE.UTF-8" # Proper date formatting while retaining english names
 unset LANGUAGE
+unset LC_ALL
