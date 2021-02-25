4e16836a3525b7e0472f3dc971ab81f4eac9f944
Florian Limberger <flo@purplekraken.com>
2021-02-25 18:24:50 +0100

    [profile.d] Fix sourcing of cargo env
    Check if `$CARGO_HOME/env` actually exists before sourcing it.

--- profile.d/rust.sh
+++ profile.d/rust.sh
@@ -1,4 +1,6 @@
 export CARGO_HOME="$HOME/lib/cargo"
 export RUSTUP_HOME="$HOME/lib/rustup"
 # The bin path is exported by $CARGO_HOME/env
-. "$CARGO_HOME/env"
+if [ -f "$CARGO_HOME/env" ]; then
+	. "$CARGO_HOME/env"
+fi
