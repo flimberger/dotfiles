127ce37be1b2cf0f9f3b38d618d8d267fa54cada
Florian Limberger <flo@purplekraken.com>
2021-03-12 18:33:35 +0100

    [tmux_session] Update config for resurgam

--- tmux_session.d/resurgam.sh
+++ tmux_session.d/resurgam.sh
@@ -2,6 +2,6 @@ newsession()
 {
 	session="$1"
 	tmux new-session -ds "$session"
-	tmux new-window -t "${session}:9" -n sysmgmt
-	tmux set-option -t "$session" status-style "fg=white,bg=black"
+	tmux new-window -t "${session}:9" -n adm
+	tmux set-option -t "$session" status-style "bg=blue,fg=blue"
 }
