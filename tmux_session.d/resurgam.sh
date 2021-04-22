3fa4740b87e28d58feb261e36357e8499070d1e1
Florian Limberger <flo@purplekraken.com>
2021-04-22 21:19:35 +0200

    [tmux_session] Update rc scripts
    Update rc scripts to follow the new convention of executing scripts in a
    subshell instead of sourcing them and running shell functions
    (https://git.sr.ht/~flimberger/tmux_session/commit/60f84f39508f1f82f5c8a39c260e158fe05ad656).

--- tmux_session.d/resurgam.sh
+++ tmux_session.d/resurgam.sh
@@ -1,7 +1,3 @@
-newsession()
-{
-	session="$1"
-	tmux new-session -ds "$session"
-	tmux new-window -t "${session}:9" -n adm
-	tmux set-option -t "$session" status-style "bg=blue,fg=blue"
-}
+tmux new-session -ds "$session"
+tmux new-window -t "${session}:9" -n adm
+tmux set-option -t "$session" status-style "bg=blue,fg=blue"
--- tmux_session.d/zodiacal.sh
+++ tmux_session.d/zodiacal.sh
@@ -1,6 +1,2 @@
-newsession()
-{
-	session="$1"
-	tmux new-session -ds "$session"
-	tmux new-window -t "${session}:9" -n sysmgmt
-}
+tmux new-session -ds "$session"
+tmux new-window -t "${session}:9" -n sysmgmt
