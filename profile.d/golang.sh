141a00a937f4aac5454423502b4e41ea6981c9ae
Florian Limberger <flo@purplekraken.com>
2021-10-08 09:25:03 +0200

    [profile] Consolidate Go environment setup
    There seem to have been two setup files for Go. That is a bit overkill
    and is now reduced to a more reasonable level of configuration.

--- profile.d/golang.sh
+++ profile.d/golang.sh
@@ -1,2 +1,10 @@
-export GOPATH="$HOME/devel/gocode"
-PATH="$GOPATH/bin:$PATH"
+# installation directory
+goroot="/usr/local/go"
+# directory containing the code
+godevel="$HOME/devel/gocode"
+
+# Variable for the tools; may have multiple entries
+export GOPATH="$godevel"
+
+# Use the code path variable, as GOPATH may contain multiple entries
+export PATH="$goroot/bin:$GOPATH/bin:$PATH"
