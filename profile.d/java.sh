988c1b1ca72a3ae094875a640cbecd1a94b637ae
Florian Limberger <flo@purplekraken.com>
2022-02-07 08:12:59 +0100

    [profile] Use system JAVA_HOME instead of Android Studio
    Also, do some cleanup:
    - Remove unnecessary quotes
    - export the JAVA_HOME variable once, after it was set in the case/esac

--- profile.d/java.sh
+++ profile.d/java.sh
@@ -1,9 +1,8 @@
 case $(uname) in
 FreeBSD)
-	export JAVA_HOME=/usr/local/openjdk8
+	JAVA_HOME=/usr/local/openjdk8
 	;;
 Linux)
-	# Older gradle versions do not support Java 10
-	# export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
-	export JAVA_HOME="$HOME/lib/apps/android-studio/jre"
+	JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 esac
+export JAVA_HOME
