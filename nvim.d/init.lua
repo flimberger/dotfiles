34ebd9257e0e55451960df9357dfc8a22c4124bf
Florian Limberger <flo@purplekraken.com>
2022-05-10 21:11:29 +0200

    [nvim] Remove powerline glyp hack
    Since I now use alacritty as primary terminal, remove the hack that
    prevents xterm from using powerline private-use area glyps. Alacritty
    knows how to render them itself (although not perfectly), so I want to
    use them. Unfortunately neovim needs the TERM variable to be
    xterm-256color to use 256 colors in tmux, otherwise I would preserve the
    hack to have it working with other terminals, too.
    
    While at it, style the dirty marker to have a slanted look, too.

--- nvim.d/init.lua
+++ nvim.d/init.lua
@@ -23,30 +23,20 @@ local function modified()
 	end
 end
 
--- workaround for xterm, which doesn't handle code points from powerline private
--- use area. U+1FB41 and U+1FB66 from "Symbols for Legacy Computing" would work,
--- too, but are also not available on my current fonts in xterm. The available
--- glyphs U+25E3 ◣ (black lower left triangle) and U+x25E5 ◥ (black upper right
--- triangle) do not fill the cells properly, so they look pretty bad.
-local sectionseparators = { left = '', right = '' }
-if os.getenv('TERM') ~= 'xterm.*' then
-	-- use some other box drawing characters
-	-- sectionseparators = { left = '░', right = '▓' }
-	sectionseparators = { left = '▙', right = '▟' }
 end
 
 require('lualine').setup {
 	options = {
 		theme = 'auto',
 		icons_enabled = false,
+		global_status = true,
 		component_separators = { left = '╲', right = '╱' },
-		--section_separators = { left = '', right = '' },
-		section_separators = sectionseparators
+		section_separators = { left = '', right = '' },
 	},
 	sections = {
 		lualine_c = {
-			{ 'filename', file_status = false },
-			{ modified, color = { bg = '#ff9900', fg = '#001122' } },
+			{ 'filename', file_status = false, separator = { right = '' } },
+			{ modified, color = { bg = '#ff9900', fg = '#001122' }, separator = { right = '' } },
 		},
 	},
 }
