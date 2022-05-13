31722b718d872f82f6d4b4b2540861063d962b7a
Florian Limberger <flo@purplekraken.com>
2022-05-13 18:26:58 +0200

    [neovim] Tune statusline
    - Use the deepsea colour palette instead of hardcoding values.
    - Add coloured fields for diagnostics.
    - Add gaps between the diagnostics fields.
    
    I have yet to understand how the process_sections() function from the
    slanted gaps example works, but this looks already quite nice.

--- nvim.d/lua/cfg/statusline.lua
+++ nvim.d/lua/cfg/statusline.lua
@@ -1,6 +1,8 @@
 -- the statusline uses the lualine package.
 -- this configuration based on the slanted gaps example from lualine
 
+local p = require('deepsea').palette
+
 local function modified()
 	if vim.bo.modified then
 		return '+'
@@ -11,18 +13,39 @@ local function modified()
 	end
 end
 
+local gap = require('lualine.component'):extend()
+function gap:draw(default_highlight)
+	self.status = ''
+	self.applied_separator = ''
+	self:apply_highlights(default_highlight)
+	self:apply_section_separators()
+	return self.status
+end
+
+local function make_gap(c)
+	return { gap, color = { fg = c, bg = c }, separator = { right = '' } }
+end
+
 require('lualine').setup {
 	options = {
 		theme = 'auto',
 		icons_enabled = false,
 		--global_status = true,
-		component_separators = { left = '╲', right = '╱' },
+		component_separators = { left = '', right = '╱' },
 		section_separators = { left = '', right = '' },
 	},
 	sections = {
+		lualine_b = {
+			'branch',
+			'diff',
+			make_gap(p.darkblue),
+			{ 'diagnostics', source = { 'nvim' }, sections = { 'error' }, diagnostics_color = { error = { bg = p.red, fg = p.white } }, separator = { right = '' }, },
+			make_gap(p.mediumblue),
+			{ 'diagnostics', source = { 'nvim' }, sections = { 'warn' }, diagnostics_color = { warn = { bg = p.orange, fg = p.white } }, separator = { right = '' }, },
+		},
 		lualine_c = {
 			{ 'filename', file_status = false, separator = { right = '' } },
-			{ modified, color = { bg = '#ff9900', fg = '#001122' }, separator = { right = '' } },
+			{ modified, color = { bg = p.orange, fg = p.darkerblue }, separator = { right = '' } },
 		},
 	},
 }
