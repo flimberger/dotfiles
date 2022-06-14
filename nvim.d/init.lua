5a9eb219fd93759df3dddddc764cdfc981a5c3e4
Florian Limberger <flo@purplekraken.com>
2022-06-14 19:04:45 +0200

    [nvim] Configure leader key
    Instead of hard-coding the space as leader, as it is currently done in
    cfg/lsp.lua, set the mapleader variable and use <Leader> instead.
    
    Also add a key binding for GFiles.

--- nvim.d/init.lua
+++ nvim.d/init.lua
@@ -10,6 +10,12 @@ vim.opt.splitright = true
 vim.opt.termguicolors = true -- enable true color support
 vim.api.nvim_command "colorscheme deepsea"
 
+-- keybindings
+vim.g.mapleader = " "
+defmapopts = { noremap = true, silent = true }
+
+vim.api.nvim_set_keymap("n", "<Leader>E", "<cmd>GFiles<CR>", defmapopts)
+
 -- more complex configuration is separated out into separate files
 require('cfg/lsp')
 require('cfg/statusline')
--- nvim.d/lua/cfg/lsp.lua
+++ nvim.d/lua/cfg/lsp.lua
@@ -1,10 +1,10 @@
 -- always available mappings
 local options = { noremap = true, silent = true }
 local map = vim.api.nvim_set_keymap
-map('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', options)
+map('n', '<Leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', options)
 map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', options)
 map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', options)
-map('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', options)
+map('n', '<Leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', options)
 
 -- mappings available with lsp client attached
 local function on_attach(client, bufn)
@@ -19,11 +19,11 @@ local function on_attach(client, bufn)
 	map(bufn, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', options)
 	map(bufn, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', options)
 	map(bufn, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', options)
-	map(bufn, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', options)
-	map(bufn, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', options)
-	map(bufn, 'n', '<space>ch', '<cmd>lua vim.lsp.buf.clear_references()<CR>', options)
-	map(bufn, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', options)
-	map(bufn, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', options)
+	map(bufn, 'n', '<Leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', options)
+	map(bufn, 'n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', options)
+	map(bufn, 'n', '<Leader>ch', '<cmd>lua vim.lsp.buf.clear_references()<CR>', options)
+	map(bufn, 'n', '<Leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', options)
+	map(bufn, 'n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', options)
 end
 
 local lsp = require('lspconfig')
