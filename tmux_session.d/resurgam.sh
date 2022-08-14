c1f34afbfd5a3b61bf8340ede39aac1f6dbda684
Florian Limberger <florian.limberger@inovex.de>
2022-08-14 19:24:40 +0200

    [tmux_session/resurgam] Fix status style

--- tmux_session.d/resurgam.sh
+++ tmux_session.d/resurgam.sh
@@ -1,3 +1,3 @@
 tmux new-session -ds "$session"
 tmux new-window -t "${session}:9" -n adm
-tmux set-option -t "$session" status-style "bg=blue,fg=blue"
+tmux set-option -t "$session" status-style "bg=blue,fg=white"
